f6782d997410666863bd7fbd5e9a12f22d27c860
Mia-acton <150271533+Mia-acton@users.noreply.github.com>
2026-09-25 11:00:10 +0000

    Tested user creation

---
+++ tests/test_users.py
@@ -0,0 +1,24 @@
+def user_payload( 
+    uid=1, 
+    name="Paul", 
+    email="paul@atu.ie", 
+    age=25, 
+    student_id="S1234567", 
+): 
+    return { 
+        "userid": uid, 
+        "name": name, 
+        "email": email, 
+        "age": age, 
+        "student_id": student_id, 
+    } 
+ 
+ 
+def test_create_user_returns_201(client): 
+    response = client.post("/api/users", json=user_payload()) 
+ 
+    assert response.status_code == 201 
+    data = response.json() 
+    assert data["userid"] == 1 
+    assert data["name"] == "Paul" 
+    assert data["email"] == "paul@atu.ie" 
